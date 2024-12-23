426ee9f1e7619d07652d1264619333787af99442
Baron Roberts <baron@cthing.com>
2024-12-22 23:04:59 -0800

    Directly use ProjectBuilder.

--- src/test/java/com/cthing/gradle/plugins/jasypt/JasyptPluginTest.java
+++ src/test/java/com/cthing/gradle/plugins/jasypt/JasyptPluginTest.java
@@ -5,30 +5,20 @@
 package com.cthing.gradle.plugins.jasypt;
 
 import org.gradle.api.Project;
-import org.junit.jupiter.api.BeforeEach;
+import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
 
 import com.cthing.gradle.plugins.test.GradleProjectAssert;
-import com.cthing.gradle.plugins.test.GradleTestProjectExtension;
 
 
 public class JasyptPluginTest {
 
-    @RegisterExtension
-    @SuppressWarnings("unused")
-    public final GradleTestProjectExtension extension = new GradleTestProjectExtension("project", "com.cthing.jasypt");
-
-    private Project project;
-
-    @BeforeEach
-    public void setUp(final Project project) {
-        this.project = project;
-    }
-
     @Test
     public void testApply() {
-        GradleProjectAssert.assertThat(this.project).hasTask("encryptString");
-        GradleProjectAssert.assertThat(this.project).hasTask("decryptString");
+        final Project project = ProjectBuilder.builder().build();
+        project.getPluginManager().apply("com.cthing.jasypt");
+
+        GradleProjectAssert.assertThat(project).hasTask("encryptString");
+        GradleProjectAssert.assertThat(project).hasTask("decryptString");
     }
 }
